0880686fa22f38597c88e6c59ec68626b4a0ca44
wellsst <wellsst@gmail.com>
2019-06-14 09:52:34 +1000

    Implement secure check - update inline docs

--- client/src/app/secure-check/secure-check.component.ts
+++ client/src/app/secure-check/secure-check.component.ts
@@ -13,6 +13,21 @@ export class SecureCheckComponent implements OnInit {
   constructor(@Inject(DOCUMENT) private document) {}
 
   ngOnInit() {
+    /*
+    The object contains info -
+    {
+      "href": "http://localhost:4200/",
+      "ancestorOrigins": {},
+      "origin": "http://localhost:4200",
+      "protocol": "http:",
+      "host": "localhost:4200",
+      "hostname": "localhost",
+      "port": "4200",
+      "pathname": "/",
+      "search": "",
+      "hash": ""
+    }
+     */
     if (!document.location.protocol.startsWith("https")) {
       this.warningMsg = "This connection is not running securely - for JWT to work securely there must be a secure connection between client and server"
     }
